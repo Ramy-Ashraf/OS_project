883ae89bd486eb5aac8b3aba0519bc7fab531eac
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2022-12-10 15:51:07 +0200

    first commit

---
+++ scheduler.c
@@ -0,0 +1,10 @@
+#include "headers.h"
+
+int main(int argc, char *argv[])
+{
+    initClk();
+
+    //TODO: implement the scheduler.
+    //TODO: upon termination release the clock resources.
+    destroyClk(true);
+}
