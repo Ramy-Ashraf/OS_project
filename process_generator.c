afa09418f99849e2f603070d837c89467f416872
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2022-12-11 22:52:25 +0200

    get input done

--- process_generator.c
+++ process_generator.c
@@ -4,23 +4,65 @@ void clearResources(int);
 
 int main(int argc, char *argv[])
 {
+    char str[100];
+    int Nlines = 0 ,i = 0;
+    char ch;
+
     signal(SIGINT, clearResources);
+    
     // TODO Initialization
     // 1. Read the input files.
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock.
-    initClk();
-    // To get time use this function. 
-    int x = getClk();
-    printf("Current Time is %d\n", x);
-    // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters.
-    // 6. Send the information to the scheduler at the appropriate time.
-    // 7. Clear clock resources
+    FILE *fp;
+    fp = fopen("processes.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Error opening file");
+        exit(1);
+    }
+
+    // get number of lines in fp but skip any line stating with "#"
+    while (fgets(str, 100, fp) != NULL)
+    {
+        if (str[0] != '#')
+            Nlines++;
+    }
+
+    // store numerical values from fp in 2d array
+    int Processes[Nlines][4];
+    fseek(fp, 0, SEEK_SET);
+    while (fgets(str, 100, fp) != NULL)
+    {
+        if (str[0] != '#')
+        {
+            sscanf(str, "%d %d %d %d", &Processes[i][0], &Processes[i][1], &Processes[i][2], &Processes[i][3]);
+            i++;
+        }
+    }
+
+    // // print Processes
+    // for (int i = 0; i < Nlines; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         printf("%d ", Processes[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+     initClk();
+     // To get time use this function. 
+     int x = getClk();
+     printf("Current Time is %d\n", x);
+     // TODO Generation Main Loop
+     // 5. Create a data structure for processes and provide it with its parameters.
+     // 6. Send the information to the scheduler at the appropriate time.
+     // 7. Clear clock resources
     
     
-    destroyClk(true);
+     destroyClk(true);
 }
 
 void clearResources(int signum)
