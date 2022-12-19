da56d0646eb5ee6c2dc4e4fb230a702ed27464e3
YoussefIsmail1337 <YoussefIsmail1337@gmail.com>
2022-12-19 11:54:00 +0000

    New branch for my changes (Reset)

--- process_generator.c
+++ process_generator.c
@@ -4,148 +4,24 @@ void clearResources(int);
 
 int main(int argc, char *argv[])
 {
-     signal(SIGINT,clearResources);	
-    //checking number of arguments entered in the terminal
-    if (argc<4)
-    {	 
-		printf("Too few arguments. Exiting!\n");         
-		exit(1);
-    }
-
     signal(SIGINT, clearResources);
-    
     // TODO Initialization
     // 1. Read the input files.
-
-    char str[100];
-    int Nlines = 0 ,i = 0;
-    char ch;
-
-    FILE *fp;
-    fp = fopen(argv[1], "r");
-    if (fp == NULL)
-    {
-        printf("Error opening file");
-        exit(1);
-        printf("\n");
-    }
-
-    // get number of lines in fp but skip any line stating with "#"
-    while (fgets(str, 100, fp) != NULL)
-    {
-        if (str[0] != '#')
-            Nlines++;
-    }
-
-    // store numerical values from fp in 2d array
-    int Processes[Nlines][4];
-    fseek(fp, 0, SEEK_SET);
-    while (fgets(str, 100, fp) != NULL)
-    {
-        if (str[0] != '#')
-        {
-            sscanf(str, "%d %d %d %d", &Processes[i][0], &Processes[i][1], &Processes[i][2], &Processes[i][3]);
-            i++;
-        }
-    }
-
-    // // print Processes
-    // for (int i = 0; i < Nlines; i++)
-    // {
-    //     for (int j = 0; j < 4; j++)
-    //     {
-    //         printf("%d ", Processes[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
-
-        char algo;
-        algo=atoi(argv[2]);
-
-        printf("The algorithm you chose is : ");
-        if(algo==1)
-        {
-            printf("Shortest job first\n");
-        }
-        else if(algo==2)
-        {
-            printf("Preemptive Highest Priority First\n");
-        }
-        else if(algo==3)
-        {
-            printf("Round Robin\n");
-        }
-        else if(algo==4)
-        {
-            printf("Multiple level Feedback Loop\n");
-        }
-
     // 3. Initiate and create the scheduler and clock processes.
-    int pid=fork();
-    int stat_loc;
-    if(pid==0) //clock
-    {
-        execv("clk.c",argv);
-    }
-    else if(pid!=-1) //process generator
-    {
-        fork();
-        if(pid==0) //schedular
-        {
-            execv("scheduler.c",argv);
-        }
-        
-    }
     // 4. Use this function after creating the clock process to initialize clock.
-
-      initClk();
-     // To get time use this function. 
-     int x = getClk();
-    //  printf("Current Time is %d\n", x);
-
-     // TODO Generation Main Loop
-     // 5. Create a data structure for processes and provide it with its parameters.
-     // 6. Send the information to the scheduler at the appropriate time.
-    key_t key_id;
-    int send_val;
-    struct msgbuff message;
-    key_id = ftok("keyfile", 65); 
-    int msgq_id = msgget(key_id, 0666 | IPC_CREAT); 
-    if (key_id == -1)
-    {
-    perror("Error in create");
-    exit(-1);
-    }
-    for(int i=0;i<Nlines;i++)
-    {
-    while(getClk()<Processes[i][1]) //checking if the process i has arrived or not
-    {}
-    //Send Schduler info of process arrived
-    message.processParameters[0]=Processes[i][0]; //sending id of process
-    message.processParameters[1]=Processes[i][1]; //sending arrival time of process
-    message.processParameters[2]=Processes[i][2]; //sending runtime of process
-    message.processParameters[3]=Processes[i][3]; //sending priority of process
-    message.mtype=Processes[i][0];
-    int send_val = msgsnd(msgq_id, &message, sizeof(message.processParameters), !IPC_NOWAIT);
-    }
-    pid = wait(&stat_loc); 
-    if(!(stat_loc & 0x00FF))
-    {
-        destroyClk(true);
-        msgctl(key_id, IPC_RMID, (struct msqid_ds *)0);
-        exit(0);
-    }
-    
+    initClk();
+    // To get time use this function. 
+    int x = getClk();
+    printf("Current Time is %d\n", x);
+    // TODO Generation Main Loop
+    // 5. Create a data structure for processes and provide it with its parameters.
+    // 6. Send the information to the scheduler at the appropriate time.
+    // 7. Clear clock resources
+    destroyClk(true);
 }
-     
-// 7. Clear clock resources
-void clearResources(int key_id)
+
+void clearResources(int signum)
 {
     //TODO Clears all resources in case of interruption
-    destroyClk(true);
-    msgctl(key_id, IPC_RMID, (struct msqid_ds *)0);
-    exit(0);
-
 }
--- scheduler.c
+++ scheduler.c
@@ -5,16 +5,7 @@ int main(int argc, char *argv[])
     initClk();
 
     //TODO: implement the scheduler.
-    key_t key_id;
-    int send_val;
-    struct msgbuff message;
-    key_id = ftok("keyfile", 65); 
-    int msgq_id = msgget(key_id, 0666 | IPC_CREAT); 
-    if (msgq_id == -1)
-    {
-        perror("Error in create");
-        exit(-1);
-    }
     //TODO: upon termination release the clock resources.
+
     destroyClk(true);
 }
