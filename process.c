85d9165ab687312b304baca755242e74bafa53b8
YoussefIsmail1337 <YoussefIsmail1337@gmail.com>
2022-12-20 10:03:18 +0000

    Started work on Process.c

--- process.c
+++ process.c
@@ -1,15 +1,12 @@
 #include "headers.h"
 
-/* Modify this file as needed*/
-int remainingtime;
-
 int main(int agrc, char *argv[])
 {
     initClk();
 
     //TODO The process needs to get the remaining time from somewhere
     //remainingtime = ??;
-    remainingtime = atoi(argv[1]);
+    int remainingtime = atoi(argv[1]);
 
     while (remainingtime > 0)
     {
